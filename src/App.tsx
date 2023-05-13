942dcf7a935613097525c9e699826b6e354c0240
nadeeraka <nadeerakan@gmail.com>
2023-05-13 12:13:58 +0530

    mui add

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import NavUi from './components/navbar/Ui'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <NavUi/>
     </>
   )
 }
