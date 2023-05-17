3cadfae59950cefdf13e6e43a8ef0da394e40ab2
nadeeraka <nadeerakan@gmail.com>
2023-05-17 13:55:53 +0530

    2nd

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
-// import './App.css'
+import './App.css'
 import NavUi from './components/navbar/Ui'
+import styled from 'styled-components';
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  const Wrapper = styled.div`
+  
+      /* background-color: #171718;
+       height: 80%;
+   */
+   
+  `;
   return (
     <>
-      <NavUi/>
+    
+    <NavUi/>
+    <Wrapper className='col1'/>
+      
     </>
   )
 }
--- src/components/navbar/Ui.tsx
+++ src/components/navbar/Ui.tsx
@@ -7,27 +7,67 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 // import { Menu as MenuIcon } from "@mui/icons-material/Menu";
+import styled from 'styled-components';
+
+
+const Wrapper = styled(AppBar)`
+.tool{
+    min-height: 60px;
+    background-color: #171718;
+    /* border-bottom:  #f8c90ad8 20px; */
+}
+
+
+
+.color{
+    height: 0px;
+}
+
+.menuItem1{
+/* width: 8%; */
+min-width:10%; 
+margin-right: 10;
+background-color:aliceblue;
+}
+
+.menuItem2{
+/* width: 80%; */
+min-width:80%;
+background-color:red;
+}
+ 
+`;
 
 export default function ButtonAppBar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+    return (
+        <Wrapper>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" className='color'  >
+                    <Toolbar className='tool'>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" className='menuItem1'>
+                            NexMovie
+                        </Typography>
+                        <Typography variant="h6" component="div"  className='menuItem2'>
+                            NexMovie
+                        </Typography>
+                        <Button  className='menuItem3' color="inherit">Login</Button>
+                    </Toolbar>
+                    <Toolbar>
+
+                        
+                       
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </Wrapper>
+    );
 }
