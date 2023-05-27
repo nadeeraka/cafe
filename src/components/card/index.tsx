b1eeddc87dd8bbaa038739eb27d607a1e5d4ff89
nadeeraka <nadeerakan@gmail.com>
2023-05-27 16:31:15 +0530

    css

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,17 +3,25 @@ import './index.css'
 export default function Card() {
   return (
     <div className='container'>
+      <div className="container-box">
       <div className="head">
-        <h1>item 1</h1>
+        <h3 className='t1'>item 1</h3>
+        <h3 className='t2'>quantity 1</h3>
       </div>
       <div className="body">
-        <p> price</p>
-        <button className=''>add</button>
-        <button>remove</button>
+<img  className='img' src="https://cdn.tasteatlas.com//images/toplistarticles/08c818739e4b48ce96d319c16f4cc0ca.jpg?mw=1300" alt="sa" height={'100%'} width={'100%'} />
+        <h3>price $9</h3>
+        <div className="button-grid ">
+        <button className='btn '>add</button>
+        <button className='btn '>remove</button>
+        </div>
+      
       </div>
       <div className="footer">
 
       </div>
+      </div>
+     
 
     </div>
   )
